6d90dd104c8d22b1ac83e8b514f74861b1693662
cbb <916643967@qq.com>
2017-03-11 17:56:51 +0800

    add README and game file

---
+++ game.py
@@ -0,0 +1,8 @@
+list_1 = []
+dict_1 = {'name':'chenbin', 'age':27}
+dict_2 = {'name':'zhangfurong', 'age':26}
+list_1.append(dict_1)
+list_1.append(dict_2)
+
+dict_1['salary'] = 6000
+dict_2['salar'] = 7000
